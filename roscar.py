80cbf977756ed65ccfbf2898e1e90508e55ebbfb
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2021-09-19 20:14:03 +0200

    intégration plus fines de direction x et y utilisation plus fine des images en diagonale

--- roscar.py
+++ roscar.py
@@ -4,34 +4,35 @@ pygame.init()
 
 # mettre à la bonne taille
 screen = pygame.display.set_mode((800, 800))
-MALIBU = (140, 198, 255)
-
-kart_face = pygame.image.load("resources/steve_kart_face.png")
-kart_face.set_colorkey(MALIBU)
-kart_droite = pygame.image.load("resources/steve_kart_droite.png")
-kart_droite.set_colorkey(MALIBU)
-kart_dos = pygame.image.load("resources/steve_kart_dos.png")
-kart_dos.set_colorkey(MALIBU)
-kart_gauche = pygame.image.load("resources/steve_kart_gauche.png")
-kart_gauche.set_colorkey(MALIBU)
-
-KART_IMAGES = []
-for f in ["resources/steve_kart_haut_gauche.png",
-          "resources/steve_kart_dos.png",
-          "resources/steve_kart_haut_droite.png",
-          "resources/steve_kart_gauche.png",
-          "resources/steve_kart_face.png",
-          "resources/steve_kart_droite.png",
-          "resources/steve_kart_bas_gauche.png",
-          "resources/steve_kart_face.png",
-          "resources/steve_kart_bas_droite.png"]:
-    image = pygame.image.load(f)
-    image.set_colorkey(MALIBU)
-    KART_IMAGES.append(image)
 
 circuit = pygame.image.load("resources/steve_kart_map.png")
 circuit = pygame.transform.scale(circuit, (800, 800))
-kart_steve = kart_dos
+
+print("image circuit chargée")
+
+MALIBU = (140, 198, 255)
+KART_IMAGES = {}
+fichiers = {
+    -1: {-1: "resources/steve_kart_haut_gauche.png",
+         0: "resources/steve_kart_dos.png",
+         1: "resources/steve_kart_haut_droite.png"},
+    0: {-1: "resources/steve_kart_gauche.png",
+        0: "resources/steve_kart_face.png",
+        1: "resources/steve_kart_droite.png"},
+    1: {-1: "resources/steve_kart_bas_gauche.png",
+        0: "resources/steve_kart_face.png",
+        1: "resources/steve_kart_bas_droite.png"}
+}
+
+for y in [-1, 0, 1]:
+    KART_IMAGES[y] = {}
+    for x in [-1, 0, 1]:
+        image = pygame.image.load(fichiers[y][x])
+        image.set_colorkey(MALIBU)
+        KART_IMAGES[y][x] = image
+        print("image kart %s,%s chargée" % (y, x))
+
+kart_steve = KART_IMAGES[-1][0]
 
 steve_position = [145, 637]
 
@@ -40,29 +41,30 @@ clock = pygame.time.Clock()
 while True:
     time = clock.tick(60)
 
-    screen.blit(circuit, (0, 0))
-    screen.blit(source=kart_steve, dest=steve_position)
-    pygame.display.flip()
-
     liste_evenements = pygame.event
 
     keys = pygame.key.get_pressed()
-    direction = 4
-    if keys[pygame.K_UP]:
-        direction -= 3
-        steve_position[1] -= 4
-    if keys[pygame.K_DOWN]:
-        direction += 3
-        steve_position[1] += 4
-    if keys[pygame.K_RIGHT]:
-        direction += 1
-        steve_position[0] += 4
-    if keys[pygame.K_LEFT]:
-        direction -= 1
-        steve_position[0] -= 4
+    axe_x = 0
+    axe_y = 0
 
-    if direction != 4:
-        kart_steve = KART_IMAGES[direction]
+    if keys[pygame.K_UP]:
+        axe_y -= 1
+    if keys[pygame.K_DOWN]:
+        axe_y += 1
+    if keys[pygame.K_LEFT]:
+        axe_x -= 1
+    if keys[pygame.K_RIGHT]:
+        axe_x += 1
+
+    steve_position[0] += axe_x * 4
+    steve_position[1] += axe_y * 4
+
+    if axe_y != 0 or axe_x != 0:
+        kart_steve = KART_IMAGES[axe_y][axe_x]
+
+    screen.blit(circuit, (0, 0))
+    screen.blit(source=kart_steve, dest=steve_position)
+    pygame.display.flip()
 
     # détection d'evenement
     for nouvel_evenement in liste_evenements.get():
