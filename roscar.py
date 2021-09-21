5d4d3ee9f06df09f05633d6de34a8158812764ac
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2021-09-21 15:19:24 +0200

    - renommage de quelques variables - joystick ignoré si pas là - utilisation d'un masque pour la collision - nouvelle image de piste avec masque - remplissage par un fond de piste

--- roscar.py
+++ roscar.py
@@ -4,13 +4,24 @@ pygame.init()
 
 # mettre à la bonne taille
 screen = pygame.display.set_mode((800, 800))
+MALIBU = (140, 198, 255)
+WHITE = (255, 255, 255)
 
-circuit = pygame.image.load("resources/steve_kart_map.png")
+circuit = pygame.image.load("resources/circuit.png").convert()
+circuit.set_colorkey(MALIBU)
+fond_piste = pygame.image.load("resources/fond_piste_sable.jpg").convert()
+fond_piste.set_colorkey(MALIBU)
+# piste = pygame.image.load("resources/piste.png").convert()
+# piste.set_colorkey(MALIBU)
+# piste = pygame.transform.scale(piste, (800, 800))
+# masque_piste.invert()
+
+# circuit.blit(source=circuit, dest=(0,0))
 circuit = pygame.transform.scale(circuit, (800, 800))
+masque_piste = pygame.mask.from_surface(circuit)
 
 print("image circuit chargée")
 
-MALIBU = (140, 198, 255)
 KART_IMAGES = {}
 fichiers = {
     -1: {-1: "resources/steve_kart_haut_gauche.png",
@@ -29,42 +40,91 @@ for y in [-1, 0, 1]:
     for x in [-1, 0, 1]:
         image = pygame.image.load(fichiers[y][x])
         image.set_colorkey(MALIBU)
+        image = pygame.transform.scale(image, (30, 30))
         KART_IMAGES[y][x] = image
         print("image kart %s,%s chargée" % (y, x))
 
-kart_steve = KART_IMAGES[-1][0]
+kart_steve: pygame.Surface = KART_IMAGES[-1][0]
+steve_masque = pygame.mask.from_surface(kart_steve)
 
-steve_position = [145, 637]
+steve_position = [145, 630]
+screen.blit(fond_piste, (0, 0))
+screen.blit(circuit, (0, 0))
+screen.blit(source=kart_steve, dest=steve_position)
+pygame.display.flip()
 
 # boucle principale
 clock = pygame.time.Clock()
+pygame.joystick.init()
+
+if pygame.joystick.get_count() > 0:
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+else:
+    joystick = None
+
 while True:
+    # on attend le framerate
     time = clock.tick(60)
 
-    liste_evenements = pygame.event
+    # détection de la direction, direction X et Y étant -1, 0, 1
 
+    # par défaut, on bouge pas
+    direction_axe_x = 0
+    direction_axe_y = 0
+
+    # detection des touches
     keys = pygame.key.get_pressed()
-    axe_x = 0
-    axe_y = 0
 
+    # direction verticale
     if keys[pygame.K_UP]:
-        axe_y -= 1
+        direction_axe_y -= 1
     if keys[pygame.K_DOWN]:
-        axe_y += 1
+        direction_axe_y += 1
+
+    # direction horizontale
     if keys[pygame.K_LEFT]:
-        axe_x -= 1
+        direction_axe_x -= 1
     if keys[pygame.K_RIGHT]:
-        axe_x += 1
+        direction_axe_x += 1
 
-    steve_position[0] += axe_x * 4
-    steve_position[1] += axe_y * 4
+    # si joystick, même chose
+    if joystick is not None:
+        joy_x = joystick.get_axis(0)
+        joy_y = joystick.get_axis(1)
 
-    if axe_y != 0 or axe_x != 0:
-        kart_steve = KART_IMAGES[axe_y][axe_x]
+        if joy_y < -0.4:
+            direction_axe_y -= 1
+        if joy_y > 0.4:
+            direction_axe_y += 1
+        if joy_x < -0.4:
+            direction_axe_x -= 1
+        if joy_x > 0.4:
+            direction_axe_x += 1
 
+    # calcule nouvelle position éventuelle
+    x = steve_position[0]
+    x += direction_axe_x * 4
+    y = steve_position[1]
+    y += direction_axe_y * 4
+
+    # détection du conflit avant application de la direction
+
+    if not steve_masque.overlap(masque_piste, (-x, -y)):  # si ça reste sur la piste on met à jour la position
+        # print("sortie de piste!")
+        steve_position[0] = x
+        steve_position[1] = y
+
+    if direction_axe_y != 0 or direction_axe_x != 0:  # si pas de mouvement, on garde l'image
+        kart_steve = KART_IMAGES[direction_axe_y][direction_axe_x]
+        steve_masque = pygame.mask.from_surface(kart_steve)
+
+    screen.blit(fond_piste, (0, 0))
     screen.blit(circuit, (0, 0))
     screen.blit(source=kart_steve, dest=steve_position)
     pygame.display.flip()
+
+    liste_evenements = pygame.event
 
     # détection d'evenement
     for nouvel_evenement in liste_evenements.get():
