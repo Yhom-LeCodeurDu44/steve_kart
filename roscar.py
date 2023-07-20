cdad36aa14f268eb10954f960eefb4d840040a01
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2023-07-20 17:15:26 +0000

    refactoré en fonctions (ça marche pas)

--- roscar.py
+++ roscar.py
@@ -7,49 +7,42 @@ screen = pygame.display.set_mode((768, 768))
 MALIBU = (140, 198, 255)
 TAILLE_KART = 20
 vitesse = VITESSE = 4
-kart_images = {
-    (0, -1): "resources/steve_kart_dos.png",
-    (1, -1): "resources/steve_kart_haut_droite.png",
-    (1, 0): "resources/steve_kart_droite.png",
-    (1, 1): "resources/steve_kart_bas_droite.png",
-    (0, 0): "resources/steve_kart_dos.png",
-    (0, 1): "resources/steve_kart_face.png",
-    (-1, 1): "resources/steve_kart_bas_gauche.png",
-    (-1, 0): "resources/steve_kart_gauche.png",
-    (-1, -1): "resources/steve_kart_haut_gauche.png",
-}
 
-for x in [-1, 0, 1]:
-    for y in [-1, 0, 1]:
-        nom_fichier = kart_images[(x, y)]
-        print(nom_fichier)
-        image = pygame.image.load(nom_fichier)
-        image.set_colorkey(MALIBU)  # color du fond a ne pas imprimer
-        kart_images[(x, y)] = pygame.transform.scale(image, (TAILLE_KART, TAILLE_KART))
+def generation_sprites_kart():
+    sprites = {
+        (0, -1): "resources/steve_kart_dos.png",
+        (1, -1): "resources/steve_kart_haut_droite.png",
+        (1, 0): "resources/steve_kart_droite.png",
+        (1, 1): "resources/steve_kart_bas_droite.png",
+        (0, 0): "resources/steve_kart_dos.png",
+        (0, 1): "resources/steve_kart_face.png",
+        (-1, 1): "resources/steve_kart_bas_gauche.png",
+        (-1, 0): "resources/steve_kart_gauche.png",
+        (-1, -1): "resources/steve_kart_haut_gauche.png",
+    }
 
-#bonne taille hors pitse
-sortie = pygame.image.load("resources/circuit.png")
-sortie.set_colorkey(MALIBU)
-sortie = pygame.transform.scale(sortie, (768, 768))
-sortie_mask = pygame.mask.from_surface(sortie)
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            nom_fichier = sprites[(x, y)]
+            print(nom_fichier)
+            image = pygame.image.load(nom_fichier)
+            image.set_colorkey(MALIBU)  # color du fond a ne pas imprimer
+            sprites[(x, y)] = pygame.transform.scale(image, (TAILLE_KART, TAILLE_KART))
     
-circuit = pygame.image.load("resources/steve_kart_map.png")
-circuit = pygame.transform.scale(circuit, (768, 768))
-direction_x = 0
-direction_y = -1
-kart_steve = kart_images[(direction_x, direction_y)]
+    return sprites
 
-# steve taille
-steve_position = [145, 637]
+def preparation_masque_hors_piste():
+    sortie = pygame.image.load("resources/circuit.png")
+    sortie.set_colorkey(MALIBU)
+    sortie = pygame.transform.scale(sortie, (768, 768))
+    mask = pygame.mask.from_surface(sortie)
+    return mask, sortie
 
-# boucle principale
-clock = pygame.time.Clock()
-while True:
-    time = clock.tick(60)
+def generation_sprite_circuit():
+    circuit = pygame.image.load("resources/steve_kart_map.png")
+    return pygame.transform.scale(circuit, (768, 768))
 
-    # détection clavier et direction + position
-    keys = pygame.key.get_pressed()
-
+def calcul_commande_direction( keys ):
     direction_x = 0
     direction_y = 0
 
@@ -65,34 +58,74 @@ while True:
     if keys[pygame.K_LEFT]:
         direction_x -= 1
 
+    return direction_x, direction_y
+
+def mise_a_jour_nouvelle_position(steve_position, direction_x, direction_y, vitesse):
     steve_position[0] += direction_x * vitesse
     steve_position[1] += direction_y * vitesse
-    
-    # arreter le kart si hors piste   
-    sortie_mask = pygame.mask.from_surface(sortie)
+    #le contenu de steve_position est directement modifié
+
+def detection_hors_piste(sortie_mask, kart_steve):
     maskart = pygame.mask.from_surface(kart_steve)
-    horspiste = sortie_mask.overlap(maskart, steve_position)
+    return sortie_mask.overlap(maskart, steve_position)
+    
+def mise_a_jour_vitesse_horspiste( horspiste ):
     if horspiste: 
-        vitesse = 0.5
         print("hors piste")
+        return 0.5
     else :
-        vitesse = VITESSE
+        return VITESSE    
+    
+def choisir_orientation_sprite_steve_kart( kart_courant, direction_x, direction_y ):     
+        # si la direction change, changer l'image
+        if direction_y != 0 or direction_x != 0:
+            return kart_images[(direction_x, direction_y)]
+        else:
+            return kart_courant
 
-
-    # si la direction change, changer l'image
-    if direction_y != 0 or direction_x != 0:
-        kart_steve = kart_images[(direction_x, direction_y)]
-
-    # dessin
-    screen.blit(circuit, (0, 0))
-    screen.blit(sortie, (0, 0))
-    screen.blit(source=kart_steve, dest=steve_position)
-    pygame.display.flip()
-
-    # détection d'evenement
+def detection_signal_interruption():
     liste_evenements = pygame.event
     for nouvel_evenement in liste_evenements.get():
         # check if the event is the X button
         if nouvel_evenement.type == pygame.QUIT:
             # if it is quit the game
             exit(0)
+
+def afficher_tout(screen, circuit, sortie, kart_steve, steve_position):
+    screen.blit(circuit, (0, 0))
+    screen.blit(sortie, (0, 0))
+    screen.blit(source=kart_steve, dest=steve_position)
+    pygame.display.flip()
+
+kart_images = generation_sprites_kart()
+circuit = generation_sprite_circuit()
+sortie_mask, sortie = preparation_masque_hors_piste()
+    
+#initialisation Steve
+direction_x = 0
+direction_y = -1
+kart_steve = kart_images[(direction_x, direction_y)]
+steve_position = [145, 637]
+
+# boucle principale
+clock = pygame.time.Clock()
+while True:
+    time = clock.tick(60)
+
+    # détection clavier et direction + position
+    keys = pygame.key.get_pressed()
+
+    direction_x, direction_y = calcul_commande_direction( keys )
+
+    mise_a_jour_nouvelle_position(steve_position, direction_x, direction_y, vitesse)
+    
+    # arreter le kart si hors piste   
+    horspiste = detection_hors_piste( sortie_mask, kart_steve)
+    vitesse = mise_a_jour_vitesse_horspiste( horspiste )
+
+    kart_steve = choisir_orientation_sprite_steve_kart(kart_steve, direction_x, direction_y)
+
+    afficher_tout( screen, circuit, sortie, kart_steve, steve_position )
+
+    # détection d'evenement
+    detection_signal_interruption()
